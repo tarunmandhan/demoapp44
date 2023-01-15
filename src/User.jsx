7ac139eecf6ceb603ee26c5d038219fc040d5359
Tarun Mandhan <mandhantarun@gmail.com>
2023-01-16 03:51:27 +0530

    useLocation()

--- src/User.jsx
+++ src/User.jsx
@@ -1,13 +1,25 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
 
 const User = () => {
   const { fname, lname } = useParams();
+  const abc = useLocation();
+  console.log(abc);
   return (
     <>
       <h1>
         User {fname} {lname} page
       </h1>
+      <h3>My current Location is - {abc.pathname}</h3>
+      {abc.pathname === `/user/tarun/mandhan` ? (
+        <button
+          onClick={() => {
+            alert("Thank you for click");
+          }}
+        >
+          Click here
+        </button>
+      ) : null}
     </>
   );
 };
